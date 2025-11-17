fff369afaa11448121477cb56677522afaa5b1a3
Aaron Yuan <ayuan1114@gmail.com>
2025-11-17 02:04:47 -0600

    remove venv starting from script (that was kinda dumb)

--- scripts/start_all.ps1
+++ scripts/start_all.ps1
@@ -17,18 +17,18 @@ Add-Content $pidFile $p1.Id
 
 
 # Start RQ Dashboard in new PowerShell window using venv Python
-${p2} = Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "[console]::Title = 'AUQA-DASHBOARD'; . $venvActivate; python -m rq_dashboard" -WindowStyle Normal -PassThru
+${p2} = Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "[console]::Title = 'AUQA-DASHBOARD'; python -m rq_dashboard" -WindowStyle Normal -PassThru
 Add-Content $pidFile $p2.Id
 
 # Start RQ Workers in new PowerShell windows sourcing venv
 for ($i = 1; $i -le $workers; $i++) {
     $title = "AUQA-WORKER-$i"
-    $cmd = "[console]::Title = '$title'; . $venvActivate; cd '$jobQueueDir'; rq worker --worker-class rq.SimpleWorker"
+    $cmd = "[console]::Title = '$title'; cd '$jobQueueDir'; rq worker --worker-class rq.SimpleWorker"
     $pw = Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", $cmd -WindowStyle Normal -PassThru
     Add-Content $pidFile $pw.Id
 }
 
 # Start Client CLI in new PowerShell window using venv Python
 Start-Sleep -Seconds 1
-${p3} = Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "[console]::Title = 'AUQA-CLIENT'; . $venvActivate; cd '$jobQueueDir'; python queue_cli.py" -WindowStyle Normal -PassThru
+${p3} = Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "[console]::Title = 'AUQA-CLIENT'; cd '$jobQueueDir'; python queue_cli.py" -WindowStyle Normal -PassThru
 Add-Content $pidFile $p3.Id
